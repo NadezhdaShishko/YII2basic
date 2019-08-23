f60d673d8df3a572655838ea80866e282b95a3ce
Shishko Nadezhda (portfolio) <naumova_nadegda@mail.ru>
2019-08-23 16:28:06 +0400

    подредактировала lesson6

---
+++ lesson1/modules/admin/views/activity/view.php
@@ -0,0 +1,60 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\DetailView;
+
+/* @var $this yii\web\View */
+/* @var $model app\modules\admin\models\Activity */
+
+$this->title = $model->title;
+$this->params['breadcrumbs'][] = ['label' => 'Activities', 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+\yii\web\YiiAsset::register($this);
+?>
+<div class="activity-view">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
+        <?= Html::a('Delete', ['delete', 'id' => $model->id], [
+            'class' => 'btn btn-danger',
+            'data' => [
+                'confirm' => 'Are you sure you want to delete this item?',
+                'method' => 'post',
+            ],
+        ]) ?>
+    </p>
+
+    <?= DetailView::widget([
+        'model' => $model,
+        'attributes' => [
+            'author.email',
+            'title',
+            'body',
+            [
+                'attribute'=>'start_date',
+                'label'=>'Дата начала активности',
+                'value'=>function($model) {
+                    return Yii::$app->formatter->asDatetime($model->start_date);
+                }
+            ],
+            [
+                'attribute'=>'end_date',
+                'label'=>'Дата окончания активности',
+                'value'=>function($model) {
+                    return Yii::$app->formatter->asDatetime($model->end_date);
+                }
+            ],
+//            'end_date:datetime',
+            [
+                'label'=>'Повторяется',
+                'value'=>function($model){
+                    return Yii::$app->formatter->asBoolean($model->cycle);
+                }
+            ],
+            'main:boolean',
+        ],
+    ]) ?>
+
+</div>
